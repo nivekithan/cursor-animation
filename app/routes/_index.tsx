171e8d72ad53f1b86022eafafd42a885b3384ce8
Nivekithan S <nivekithan363@gmail.com>
2024-06-05 22:00:37 +0530

    Add first cursor based animation

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,26 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useMemo } from "react";
+import { Cursor } from "~/components/cursor";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Cursor Animation" },
+    { name: "description", content: "EPYC cursor animation" },
   ];
 };
 
 export default function Index() {
+  const listOfCursors = useMemo(() => {
+    return Array(100)
+      .fill(0)
+      .map((_, i) => {
+        return <Cursor key={i} />;
+      });
+  }, []);
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="h-screen flex items-center p-40 flex-wrap max-w-[1520px]">
+      {listOfCursors}
     </div>
   );
 }
